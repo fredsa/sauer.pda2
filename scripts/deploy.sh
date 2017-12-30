97b8f3a67cbcb13601d875974e5ae7dc76d140d7
Fred Sauer <fredsa@gmail.com>
2017-12-29 16:17:50 -0800

    fix deploy.sh gcloud test on windows

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,29 +12,23 @@ git status
 echo
 echo -e "Hit [ENTER] to continue: \c"
 read
+echo
 
-SCRIPTS_DIR="$( dirname $0 )"
-ROOT_DIR="$( dirname $SCRIPTS_DIR )"
+os_name="$( uname -s )"
+case $os_name in
+  Darwin|Linux)
+    GCLOUD="gcloud"
+    ;;
+  MINGW64*)
+    GCLOUD="gcloud.cmd"
+    ;;
+  *)
+    echo "ERROR: Unknown OS name for `$os_name`" 1>&2
+    exit 1
+    ;;
+esac
 
-GCLOUD="$(which gcloud)" \
-  || (echo "ERROR: gcloud must be in your PATH"; exit 1)
-while [ -L "$GCLOUD" ]
-do
-  GCLOUD="$(readlink $GCLOUD)"
-done
-
-BIN_DIR="$(dirname $GCLOUD)"
-
-if [ "$(basename $BIN_DIR)" == "bin" ]
-then
-  SDK_HOME=$(dirname $BIN_DIR)
-  if [ -d $SDK_HOME/platform/google_appengine ]
-  then
-    SDK_HOME=$SDK_HOME/platform/google_appengine
-  fi
-else
-  SDK_HOME=$BIN_DIR
-fi
+$GCLOUD -v || (echo "ERROR: gcloud must be in your PATH"; exit 1)
 
 PROJECT=sauer-pda
 echo

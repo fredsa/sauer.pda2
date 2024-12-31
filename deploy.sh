38900178e932e787a6b0d30caac3b34ea75c9aef
Fred Sauer <fredsa@google.com>
2024-12-31 14:42:04 -0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+set -ue
+
+VERSION="$( git log -1 --pretty=format:%H )"
+if [ -n "$( git status --porcelain )" ]
+then
+  VERSION="dirty-$VERSION"
+fi
+
+git status
+echo
+echo -e "Hit [ENTER] to continue: \c"
+read
+echo
+
+os_name="$( uname -s )"
+case $os_name in
+  Darwin|Linux)
+    GCLOUD="gcloud"
+    ;;
+  MINGW64*)
+    GCLOUD="gcloud.cmd"
+    ;;
+  *)
+    echo "ERROR: Unknown OS name for `$os_name`" 1>&2
+    exit 1
+    ;;
+esac
+
+$GCLOUD -v || (echo "ERROR: gcloud must be in your PATH"; exit 1)
+
+echo -e "\n*** DEPLOYING ***\n"
+$GCLOUD app deploy --version "${VERSION}"
